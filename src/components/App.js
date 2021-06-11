355852c3b16160d51b820cedfa31e752df8eae2b
radisahussein <radisahrachmadi@gmail.com>
2021-06-11 13:18:13 +0700

    landing page in

--- src/components/App.js
+++ src/components/App.js
@@ -18,9 +18,8 @@ import PrivateRoute from './PrivateRoute'
 function App() {
   return(
     
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight:"100vh"}}>
       
-      <div className="w-100" style={{maxWidth:'400px'}}>
+      <div className="w-100" >
         
         <Router>
           <AuthProvider>
@@ -35,7 +34,6 @@ function App() {
           </AuthProvider>
         </Router>
       </div>
-    </Container>
     
   )
   
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,87 +1,59 @@
 import React, { Component } from "react";
 import './LandingPage.css';
 
+
 class LandingPage extends Component {
-    
+
     render() {
         return (
             <div>
-                <ul>
-                    <li>About</li>
-                    <li>Features</li>
-                    <li>Contact Us</li>
-                    <li>Login/Signup</li>
-                </ul>
-
-                <div className="bgimg-1">
-                    <div className="caption">
-                        <span className="home">We Are BuqMarq</span>
-                    </div>
-                </div>
-
-                <div className="about-us">
-                    <h3>About Us</h3>
-                    <p>Nasectur per nec posuer terpus ,enroajdsioajdoaijsdi</p>
-                </div>
-                {/* <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
-                    <h3 className="about" style="text-align:center;">About Us</h3>
-                    <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-                </div> */}
-                
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>BuqMarq</title>
+            <link rel="stylesheet" href="LandingPage.css" />
+            <link rel="icon" href="buqmarq logo.png" />
+            <ul>
+              <li><a href="#about">About</a></li>
+              <li><a href="#feature">Features</a></li>
+              <li><a href="#contact">Contact Us</a></li>
+              <li style={{"float":"right"}}><a href="/login">Login/Sign up</a></li>
+            </ul>
+            <div className="bgimg-1">
+              <div className="caption">
+                <span id="home" className="border">We are BuqMarq</span>
+              </div>
             </div>
-
-            
-            // <body>
-            //     <ul className="landing">
-            //         <li className="landing"><a href="#about">About</a></li>
-            //         <li><a href="#feature">Features</a></li>
-            //         <li><a href="#contact">Contact Us</a></li>
-            //         <li style="float:right"><a href="#">Login/Sign up</a></li>
-            //     </ul>
-                        
-            // <div class="bgimg-1">
-            // <div class="caption">
-            // <span id="home" class="border">We are BuqMarq</span>
-            // </div>
-            // </div>
-            
-            // <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
-            // <h3 id="about" style="text-align:center;">About Us</h3>
-            // <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-            // </div>
-            
-            // <div class="bgimg-2">
-            // <div class="caption">
-            // <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Browse Bookmarks Like Never Before</span>
-            // </div>
-            // </div>
-            
-            // <div style="position:relative;">
-            // <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-            // <h3 id="feature" style="color: white;text-align:center;">Features</h3>
-            // <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-            // </div>
-            // </div>
-            
-            // <div class="bgimg-3">
-            // <div class="caption">
-            // <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Innovative and Inspirational</span>
-            // </div>
-            // </div>
-            
-            // <div style="position:relative;">
-            // <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
-            // <h3 id="contact" style="color: white;text-align:center;">Contact Us</h3>
-            // <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
-            // </div>
-            // </div>
-            
-            // <div class="bgimg-1">
-            // <div class="caption">
-            //     <span class="border"><a href="#">Sign up Now!</a></span>
-            // </div>
-            // </div>
-            // </body>
+            <div style={{"color":"#777","background-color":"white","-webkit-text-align":"justify","text-align":"justify","padding":"50px 80px"}}>
+              <h3 id="about" style={{"-webkit-text-align":"center","text-align":"center"}}>About Us</h3>
+              <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+            </div>
+            <div className="bgimg-2">
+              <div className="caption">
+                <span className="border" style={{"background-color":"transparent","font-size":"25px","color":"#f7f7f7"}}>Browse Bookmarks Like Never Before</span>
+              </div>
+            </div>
+            <div style={{"position":"relative"}}>
+              <div style={{"color":"#ddd","background-color":"#282E34","-webkit-text-align":"justify","text-align":"justify","padding":"50px 80px"}}>
+                <h3 id="feature" style={{"color":"white","-webkit-text-align":"center","text-align":"center"}}>Features</h3>
+                <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+              </div>
+            </div>
+            <div className="bgimg-3">
+              <div className="caption">
+                <span className="border" style={{"background-color":"transparent","font-size":"25px","color":"#f7f7f7"}}>Innovative and Inspirational</span>
+              </div>
+            </div>
+            <div style={{"position":"relative"}}>
+              <div style={{"color":"#ddd","background-color":"#282E34","-webkit-text-align":"justify","text-align":"justify","padding":"50px 80px"}}>
+                <h3 id="contact" style={{"color":"white","-webkit-text-align":"center","text-align":"center"}}>Contact Us</h3>
+                <p>Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+              </div>
+            </div>
+            <div className="bgimg-1">
+              <div className="caption">
+                <span className="border"><a href="/login">Sign up Now!</a></span>
+              </div>
+            </div>
+          </div>
         );
     };
 }
